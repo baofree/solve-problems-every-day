dc7f19ca1cee2a77b267761486aa2c6a079ef552
baofree <baofree.wook@gmail.com>
2018-11-04 06:18:28 +0900

    LeetCode 234
    - improve(palindrome linked list)

--- leet_code/easy/234_palindrome-linked-list.py
+++ leet_code/easy/234_palindrome-linked-list.py
@@ -33,8 +33,36 @@ class Solution(object):
             head = head.next
         return stack == queue
 
+    def isPalindrome1(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        stack = []
+
+        worker = head
+        runner = head
+
+        while runner is not None and runner.next is not None:
+            stack.append(worker.val)
+            worker = worker.next
+            runner = runner.next.next
+        if runner:
+            worker = worker.next
+        while worker:
+            if stack.pop() != worker.val:
+                return False
+
+            worker = worker.next
+
+        return True
+
 
 if __name__ == '__main__':
     solution = Solution()
     print(solution.isPalindrome(helper.gen_linked_list([1, 2])))
     print(solution.isPalindrome(helper.gen_linked_list([1, 2, 2, 1])))
+
+    # print(solution.isPalindrome1(helper.gen_linked_list([1, 2, 3, 2, 1])))
+    print(solution.isPalindrome1(helper.gen_linked_list([1, 2, 3, 3, 4, 1])))
+    print(solution.isPalindrome1(helper.gen_linked_list([])))
