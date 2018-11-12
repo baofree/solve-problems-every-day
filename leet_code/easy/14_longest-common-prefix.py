c68738d62b96c3bce545c77cf6b0c440e1fa625b
baofree <baofree.wook@gmail.com>
2018-11-12 13:15:00 +0900

    LeetCode 14
    - longest common prefix

---
+++ leet_code/easy/14_longest-common-prefix.py
@@ -0,0 +1,43 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Note:
+All given inputs are in lowercase letters a-z.
+
+"""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        if len(strs) == 0:
+            return ""
+        result = strs[0]
+        for index in range(1, len(strs)):
+            while strs[index].find(result) != 0:
+                result = result[:-1]
+                if not result:
+                    return ""
+        return result
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.longestCommonPrefix(["flower", "flow", "flight"]))
+    print(solution.longestCommonPrefix(["dog", "racecar", "car"]))
